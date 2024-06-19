e2129ca95e7c0e585d7e58b3f26a53d90835dee3
Nemanja Radić <nemanjaradic234@gmail.com>
2024-06-19 21:27:19 +0200

    Filtriranje, ali nije uradjeno skroz

--- web-shop/front/src/views/GuestUserView.vue
+++ web-shop/front/src/views/GuestUserView.vue
@@ -6,12 +6,19 @@ export default {
   data() {
     return {
       proizvodi: [],
-      query: ''
+      query: '',
+      kategorije: [],
+      cenaMin: '',
+      cenaMax: '',
+      naziv: '',
+      tipProdaje: ''
     };
   },
   mounted() {
     this.getProizvodi();
     this.searchProizvodi();
+    this.getKategorije();
+    this.filterProizvodi();
   },
   methods: {
     getProizvodi() {
@@ -33,6 +40,26 @@ export default {
           console.log(error);
           alert("Ne postoje proizvodi sa tim nazivom!");
         });
+    },
+    getKategorije() {
+      axios.get('http://localhost:8081/kategorije', {withCredentials: true})
+      .then((response) => {
+        this.kategorije = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Ne postoji kategorija!");
+      })
+    },
+    filterProizvodi() {
+      axios.get(`http://localhost:8081/products/filter?tipProdaje=${this.tipProdaje}`, {withCredentials: true})
+        .then((response) => {
+          this.proizvodi = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Nema proizvoda!");
+        })
     }
   }
 };
@@ -56,7 +83,7 @@ export default {
         </ul>
         <form class="d-flex" role="search" @submit.prevent="searchProizvodi">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="query">
-          <button class="btn btn-outline-success" v-on:click="searchProizvodi">Search</button>
+          <button class="btn btn-outline-success">Search</button>
         </form>
         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
           <button id="loginBtn" class="btn btn-primary me-md-2" style="margin-left: 40px" type="button">Login</button>
@@ -66,14 +93,53 @@ export default {
     </div>
   </nav>
 
-  <div><h2 class="proizvod">Proizvodi</h2></div>
-  <div class="card-deck">
-    <div v-for="proizvod in proizvodi" class="card " style="width: 18rem;">
-      <img :src="proizvod.profilnaURL" class="card-img-top" alt="slika">
-      <div class="card-body">
-        <h5 class="card-title">{{proizvod.naziv}}</h5>
-        <p class="card-text">{{proizvod.opis}}</p>
-        <a href="#" class="btn btn-primary">Vidi još</a>
+  <div class="page">
+    <div class="filter">
+      <form @submit.prevent="filterProizvodi">
+        <div class="mb-4">
+          <label for="minPrice" class="form-label">Najmanja cena:</label>
+          <input type="number" class="form-control" id="minPrice" name="minPrice" placeholder="Cena od..." v-model="cenaMin">
+        </div>
+
+        <div class="mb-4">
+          <label for="maxPrice" class="form-label">Najveća cena:</label>
+          <input type="number" class="form-control" id="maxPrice" name="maxPrice" placeholder="Cena do..." v-model="cenaMax">
+        </div>
+
+        <div class="mb-4">
+          <label class="form-label">Kategorije:</label>
+          <div v-for="kategorija in kategorije" class="form-check">
+            <input class="form-check-input" type="radio" name="category" id="{{kategorija.id}}" :value="kategorija.naziv" v-model="naziv" >
+            <label class="form-check-label" for="{{kategorija.naziv}}">{{ kategorija.naziv }}</label>
+          </div>
+        </div>
+
+        <div class="mb-4">
+          <label class="form-label">Način prodaje:</label>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="sellingType" id="fiksnaCena" value="FIKSNA_CENA" v-model="tipProdaje">
+            <label class="form-check-label" for="fiksnaCena">Fiksna cena</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="sellingType" id="aukcija" value="AUKCIJA" v-model="tipProdaje">
+            <label class="form-check-label" for="aukcija">Aukcija</label>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Filter</button>
+      </form>
+    </div>
+    <div class="proizvodi-container">
+      <div><h2 class="proizvod">Proizvodi</h2></div>
+      <div class="card-deck">
+        <div v-for="proizvod in proizvodi" class="card " style="width: 18rem;">
+          <img :src="proizvod.profilnaURL" class="card-img-top" alt="slika">
+          <div class="card-body">
+            <h5 class="card-title">{{proizvod.naziv}}</h5>
+            <p class="card-text">{{proizvod.opis}}</p>
+            <h5 class="card-text"><b>Cena: {{proizvod.cena}} din.</b></h5>
+            <a href="#" class="btn btn-primary">Vidi još</a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -130,6 +196,24 @@ export default {
 
 .btn-primary:hover {
   background-color: #12613e;
+}
+
+.page {
+  margin: 1% 0;
+  display: flex;
+  width: 100%;
+}
+
+.filter {
+  display: table-column;
+  padding: 2%;
+  position: fixed;
+}
+
+.proizvodi-container {
+  margin-left: 25em;
+  width: 100%;
+  flex-grow: 1;
 }
 
 footer {
